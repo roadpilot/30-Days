f7524f0c35d50dc9334966190e9c0e2150225190
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-06-28 17:43:03 +0000

    day 2

--- index.js
+++ index.js
@@ -28,3 +28,11 @@ function main() {
     // The 's' variable above should be printed first.
     console.log(s + s2)
 }
+
+// DAY 2
+function solve(meal_cost, tip_percent, tax_percent) {
+    // Write your code here
+    let tip = meal_cost*(tip_percent/100);
+    let tax = meal_cost*(tax_percent/100);
+    console.log(Math.round(meal_cost+tip+tax))
+}

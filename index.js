9c86f0876e39fb6473b8e5202639614edf8c3160
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-06-30 19:01:00 +0000

    day 4 js class instances

--- index.js
+++ index.js
@@ -57,3 +57,30 @@ function main() {
     //     console.log("Not Weird");
     // }
 }
+
+// DAY 4
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+    if (initialAge>=0){this.age = initialAge}
+    else {
+        this.age = 0;
+        console.log("Age is not valid, setting age to 0.")
+    }
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+    switch(true) {
+        case (this.age < 13):
+            console.log("You are young.")
+            break;
+        case (this.age < 18):
+            console.log("You are a teenager.")
+            break;
+        default:
+            console.log("You are old.")
+    }
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+        this.age++;
+   };
+}

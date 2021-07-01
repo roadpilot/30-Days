32296a516d9140e80a1572d31031ef983d9cc50d
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-07-01 21:19:24 +0000

    day 5 loops

--- index.js
+++ index.js
@@ -84,3 +84,11 @@ function Person(initialAge){
         this.age++;
    };
 }
+
+// DAY 5
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    for (let i=1; i<=10; i++){
+        console.log(`${n} x ${i} = ${n*i}`);
+    }
+}

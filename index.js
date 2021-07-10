2e02eb085ea469b812041d42aa28b7bf8a6cdb8b
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-07-10 16:01:48 +0000

    day 14 python class and scope

--- index.js
+++ index.js
@@ -238,3 +238,28 @@ class MyBook extends Book {
         console.log(`Price: ${this.price}`)
     }
 // End class
+
+// DAY 14 (HAD TO JUMP TO PYTHON FOR THIS CHALLENGE)
+class Difference:
+    def __init__(self, a):
+        self.__elements = a
+
+    def computeDifference(self):
+        min = 101
+        max = 0 #per constraints
+        for item in self.__elements:
+            if item < min:
+                min = item
+            if item > max:
+                max = item
+        self.maximumDifference = max - min 
+
+# End of Difference class
+
+_ = input()
+a = [int(e) for e in input().split(' ')]
+
+d = Difference(a)
+d.computeDifference()
+
+print(d.maximumDifference)

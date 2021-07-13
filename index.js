9d29af18566ce857ebe1c9dadf68730c0fdcd283
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-07-13 16:20:18 +0000

    day 17 more exceptions

--- index.js
+++ index.js
@@ -311,3 +311,17 @@ function main() {
         console.log('Bad String')
     }
 }
+
+// DAY 17 MORE EXCEPTIONS
+class Calculator{
+    constructor(){
+    }
+    power(n,p){
+        if (n<0 || p<0){
+            return "n and p should be non-negative"
+        }
+        else {
+            return (n**p)
+        }
+    }
+}

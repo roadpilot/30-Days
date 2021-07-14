913955ec706aa0b539d684d1ad41b071d76f3fee
Martin Achtermann <65299501+roadpilot@users.noreply.github.com>
2021-07-14 17:45:02 +0000

    DAY 18 PALINDROME

--- index.js
+++ index.js
@@ -325,3 +325,54 @@ class Calculator{
         }
     }
 }
+
+// DAY 18 PALINDROME
+function Solution(){
+  //Write your code here
+  let stack = []
+  let queue = []
+  this.pushCharacter = function(a){
+    stack.push(a)   
+  }
+  this.enqueueCharacter = function(a){
+    queue.push(a)
+  }
+  this.popCharacter = function(){
+    return stack.pop()
+  }
+  this.dequeueCharacter = function(){
+    return queue.shift()
+  }
+}
+
+function main(){
+    // read the string s
+    var s=readLine();
+    var len=s.length;
+    // create the Solution class object p
+    var obj=new Solution();
+    //push/enqueue all the characters of string s to stack
+    for(var i=0;i<len;i++){
+        obj.pushCharacter(s.charAt(i));
+        obj.enqueueCharacter(s.charAt(i));
+    }
+  
+    var isPalindrome=true;
+    /*
+    pop the top character from stack
+    dequeue the first character from queue
+    compare both the characters*/
+
+    for(var i=0;i<len/2;i++){
+        if(obj.popCharacter()!=obj.dequeueCharacter()){
+            isPalindrome=false;
+          	break;
+        }
+    }
+    //finally print whether string s is palindrome or not
+
+    if(isPalindrome)
+        console.log("The word, "+s+", is a palindrome.");    
+    else
+        console.log("The word, "+s+", is not a palindrome.");
+}
